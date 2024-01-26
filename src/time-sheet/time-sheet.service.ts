b7e45e56abfaa2117b93d9b9b1024f16673d7da9
_JULZ <julz101.pci@gmail.com>
2024-01-26 11:39:25 +0800

    refactor: minimize sentence in attendance

--- src/time-sheet/time-sheet.service.ts
+++ src/time-sheet/time-sheet.service.ts
@@ -149,11 +149,11 @@ export class TimeSheetService {
         if (attendance.length === 1) return `${motivation}.\nNot all heroes wear capes! Solo yern?\n1. ${attendance[0].username}, Logged @ ${moment(attendance[0].timeIn).format("hh:mm A")} - Expected logout @ ${moment(attendance[0].expectedTimeOut).format("hh:mm A")} <:superhero:123456789012345678>`;
 
         if (attendance.length > 1) {
-            let result = `${motivation}\n`;
+            let result = `${motivation}\n Expected Time Ranges <:clock9:123456789012345678>.\n`;
             attendance.forEach((item, index) => {
                 const timeIn = moment(item.timeIn).format("hh:mm A");
                 const expectedTimeOut = item.expectedTimeOut ? moment(item.expectedTimeOut).format("hh:mm A") : item.signatureDate;
-                result += `${index + 1}. ${item.username}, Expected time range: ${timeIn} - ${expectedTimeOut}.\n`;
+                result += `${index + 1}. ${item.username}, ${timeIn} - ${expectedTimeOut}.\n`;
 
             });
             return result;
